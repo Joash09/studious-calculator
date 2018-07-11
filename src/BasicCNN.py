8433226c1525b4d12372504be8e29b4562829f55
joash09 <joash.n09@gmail.com>
2018-07-11 23:54:43 +0200

    Different types filters have been added and work

--- src/BasicCNN.py
+++ src/BasicCNN.py
@@ -29,26 +29,41 @@ def ReLU(x):
     return 0
 
 def max_pool(img, region):
+    
     down_sample = np.zeros([int(img.shape[0]/region[0]), int(img.shape[0]/region[1])])
     for x in range(0, img.shape[0], region[0]):
         for y in range(0, img.shape[1], region[1]):
-            #print(x, y)
             max_val = np.max(img[x:x+region[0], y:y+region[1]])
             xcord = int(x/region[0])
             ycord = int(y/region[1])
             down_sample[xcord][ycord] = max_val
+    
     return down_sample
+
+#Different types of filters
+#==========================
+identity = np.array([[0,0,0],[0,1,0],[0,0,0]])
+edge_detection1 = np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
+edge_detection2 = np.array([[-1,-1,-1], [-1,-8,-1], [-1,-1,-1]])
+edge_detection3 = np.array([[1,0,-1], [0,0,0], [-1,0,1]]) #Works really well
+sharpen = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])
 
 image = cv2.imread('opencv_logo.png')
 b,r,g = cv2.split(image)
-#print(g[500, 500])
-region = [100,100]
-print(int(200/region[0]))
-print(max_pool(g, region))
-
+plt.imshow(b)
+plt.show()
+region = [25,25]
+down_sample = max_pool(b, region)
+plt.imshow(down_sample)
+plt.show()
+f1 = np.eye(10)
+out = convolve(down_sample, edge_detection3)
+print(out)
+plt.imshow(out)
+plt.show()
 
 #kernal = np.eye(50)
 #resolution_map = convolve(b, kernal)
 #transform = ReLU(resolution_map)
 #plt.imshow(transform)
-plt.show()
+#plt.show()
