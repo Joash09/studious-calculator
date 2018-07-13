088194fe51b44e653b22623957e90b8a00b04989
Joash09 <joash.n09@gmail.com>
2018-07-13 11:51:30 +0200

    Convolution is done. Max pooling isn't working very well. Use new images for testing

--- src/BasicCNN.py
+++ src/BasicCNN.py
@@ -7,7 +7,7 @@ def convolve(image, kernal, stride):
     padded_kernal = np.zeros(image.shape)
     padding = np.array(image.shape)-np.array(kernal.shape)
     resolution_map = np.zeros(padding)
-
+    print(resolution_map.shape)
     for x_shift in range(0, padding[0], stride):
         for y_shift in range(0, padding[1], stride):
             #Shifting the kernal
@@ -19,18 +19,17 @@ def convolve(image, kernal, stride):
             #Do convolution
             temp = np.sum(np.multiply(image, padded_kernal))
             resolution_map[x_shift][y_shift] = temp
-            
             #Reset the padded kernal
             padded_kernal = np.zeros(image.shape)
     
     return resolution_map.astype(np.uint8)
 
-def convolve_color(layers, kernal, stride):
-    convolved_image = []
-    for p in layers:
-        convolved_image.append(convolve(p, kernal, stride).astype(np.uint8))
-        print("Done")
-    return cv2.merge((convolved_image[0], convolved_image[1], convolved_image[2]))
+def convolve_color(image, kernal, stride):
+    b,g,r = cv2.split(image)
+    b = convolve(b, kernal, stride)
+    g = convolve(g, kernal, stride)
+    r = convolve(r, kernal, stride)
+    return cv2.merge((b,g,r))
 
 def ReLU(x):
     if np.where(x>0):
@@ -38,43 +37,55 @@ def ReLU(x):
     return 0
 
 def max_pool(img, region):
-    
-    down_sample = np.zeros([int(img.shape[0]/region[0]), int(img.shape[1]/region[1])])
+    down_sample = np.zeros([11,11])
+    #down_sample = np.zeros([int(img.shape[0]/region[0]), int(img.shape[1]/region[1])])
     for x in range(0, img.shape[0], region[0]):
         for y in range(0, img.shape[1], region[1]):
-            max_val = np.max(img[x:x+region[0], y:y+region[1]])
+            max_val = np.average(img[x:x+region[0], y:y+region[1]])
             xcord = int(x/region[0])
             ycord = int(y/region[1])
             down_sample[xcord][ycord] = max_val
     return down_sample.astype(np.uint8)
 
-def max_pool_color(layers, region):
-    image = []
-    for layer in layers:
-        image.append(max_pool(layer, region))
-    return cv2.merge((image[0], image[1], image[2]))
+def max_pool_color(img, region):
+    b,g,r = cv2.split(img)
+    b = max_pool(b, region)
+    g = max_pool(g, region)
+    r = max_pool(r, region)
+    return cv2.merge((b,g,r))
 
 #Different types of filters
 #==========================
 identity = np.array([[0,0,0],[0,1,0],[0,0,0]])
 edge_detection1 = np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
-edge_detection2 = np.array([[-1,-1,-1], [-1,-8,-1], [-1,-1,-1]])
+edge_detection2 = np.array([[-1,-1,-1], [-1,-8,-1], [-1,-1,-1]])#Rubbish
 edge_detection3 = np.array([[1,0,-1], [0,0,0], [-1,0,1]]) #Works really well
 sharpen = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])
 guassion_blur = np.array([[1,2,1], [2,4,2],[1,2,1]])*(1/16)
 
 
-
 #Test Code
 #=========
-image = cv2.imread('opencv_logo.png')
-b,g,r = cv2.split(image)
+image = cv2.imread('test2.jpg', 0)
+#plt.show()
+#plt.plot()
 
-new_im = max_pool_color((b,g,r), [10,10])
-plt.imshow(new_im)
+plt.figure(1)
+#Edge dectection 1
+resolution_1 = convolve(image, edge_detection1, 1)
+plt.subplot(211)
+plt.imshow(resolution_1)
+
+#Edge dectection 3
+resolution_2 = convolve(image, edge_detection2, 1)
+plt.subplot(212)
+plt.imshow(resolution_2)
 plt.show()
 
-new_im = convolve(b, edge_detection3, 1)
-#new_im = convolve_color((b,r,g), edge_detection3, 1)
-plt.imshow(new_im)
+#Max Pool
+#resolution_2 = convolve(resolution_2,guassion_blur,1)
+resolution_2 = ReLU(resolution_2)
+print(resolution_2)
+plt.figure(2)
+plt.imshow(resolution_2)
 plt.show()
